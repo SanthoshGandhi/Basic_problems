bd8c521cbacfd6cc792af60f23c8744c5e84c3d1
SanthoshGandhi <74761021+SanthoshGandhi@users.noreply.github.com>
2020-12-17 21:36:40 +0530

    Handling Files
    Handling files in directories in the console.

---
+++ File_and_directories.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Files_and__Directories1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                string path = "D:\\sample\\";
+
+            //File in a diectory
+            Console.WriteLine(getFilesInDirectory(path));
+
+            //copy files-overWrite if exists
+            FileInfo f = new FileInfo(path + "Input.txt");
+            f.CopyTo(path + "NewInput.txt", true);
+            Console.WriteLine("copy Successfull");
+
+            //rename files
+            FileInfo fnew = new FileInfo(path + "NewInput.txt");
+            f.MoveTo(path + "renamedNewInput.txt");
+            Console.WriteLine("Rename Successfull");
+
+        }
+        private static string getFilesInDirectory(string path)
+        {
+            string FileDetails = "";
+            DirectoryInfo dir = new DirectoryInfo(path);
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                FileDetails += file.Name + "\t" + file.Length + "byte \t" + file.CreationTime + "\n";
+            }
+            return FileDetails;
+        }
+        
+    }
+}
